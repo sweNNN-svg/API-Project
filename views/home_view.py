0713bf59e534e878e4e6a9252a611dd8079cf761
sweNNN-svg <emre.ofleep@gmail.com>
2023-11-12 22:50:01 +0300

    home_view.py düzenleme

--- views/home_view.py
+++ views/home_view.py
@@ -1,8 +1,23 @@
+# views/home_view.py
+
 from tkinter import *
+import requests
 
 def open_home_view():
-    homeWindow = Tk()
-    homeWindow.title('Home')
-    homeWindow.geometry('500x500')
-    Label(homeWindow, text="This is a Home Window").pack()
-    homeWindow.mainloop()
+    root = Tk()
+    root.title('Home')
+    root.geometry('500x500')
+
+    # API çağrısı
+    api_key = 'API_ANAHTARINIZ'
+    city = 'ISTANBUL'
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+
+    response = requests.get(url)
+    data = response.json()
+
+    # API'den gelen verileri işleyerek ekrana bastırma
+    Label(root, text=f'Hava Durumu: {data["weather"][0]["description"]}').pack()
+    Label(root, text=f'Sıcaklık: {data["main"]["temp"]} Kelvin').pack()
+
+    root.mainloop()
